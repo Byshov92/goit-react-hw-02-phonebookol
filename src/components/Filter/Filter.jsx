c88224bc3f9ef9d87ff7ea5bb656b40ab8f4d9a1
Byshov92 <dimabyshov@gmail.com>
2023-12-31 14:40:46 +0200

    fin

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,14 @@
+import { LabelFilter } from './Filter.styled';
+
+export const Filter = ({ filterInputValue, onChangeInputFilter }) => {
+  return (
+    <LabelFilter>
+      <span>Find contacts by name</span>
+      <input
+        type="text"
+        value={filterInputValue}
+        onChange={onChangeInputFilter}
+      />
+    </LabelFilter>
+  );
+};
